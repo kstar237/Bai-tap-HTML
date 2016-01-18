e0024313f64b75346b5fdfb2b421e58fdd7545b1
kstar237 <kstar237@gmail.com>
2016-01-18 11:17:27 +0700

    Ai la trieu phu

---
+++ Ask-and-Answer/javascript.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kien237 on 1/18/16.
+ */
+function greats(){
+ document.getElementById("great").style.visibility = "visible";
+}
+function bads(){
+    document.getElementById("bad").style.visibility = "visible"
+}
